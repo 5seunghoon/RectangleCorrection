092a3cf95819af3d0a23dbfcb26fff31919fb616
Seunghoon Oh <stdsalmon@gmail.com>
2019-04-19 16:56:04 +0900

    Feat : Convert selected image to gray image

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,11 +7,38 @@ using namespace cv;
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_tistory_deque_rectanglecorrection_camera_CameraActivity_convertRGBtoGray(JNIEnv *env,
-                                                                            jobject instance,
-                                                                            jlong matAddrInput,
-                                                                            jlong matAddrResult) {
+                                                                                  jobject instance,
+                                                                                  jlong matAddrInput,
+                                                                                  jlong matAddrResult) {
     // 입력 RGBA 이미지를 GRAY 이미지로 변환
-    Mat &matInput = *(Mat *)matAddrInput;
-    Mat &matResult = *(Mat *)matAddrResult;
+    Mat &matInput = *(Mat *) matAddrInput;
+    Mat &matResult = *(Mat *) matAddrResult;
     cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tistory_deque_rectanglecorrection_rect_RectCorrectionActivity_loadImage(JNIEnv *env,
+                                                                                   jobject instance,
+                                                                                   jstring imageFileName_,
+                                                                                   jlong img) {
+    const char *imageFileName = env->GetStringUTFChars(imageFileName_, 0);
+    Mat &imageInput = *(Mat *) img;
+    imageInput = imread(imageFileName, IMREAD_COLOR);
+
+    env->ReleaseStringUTFChars(imageFileName_, imageFileName);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tistory_deque_rectanglecorrection_rect_RectCorrectionActivity_imageProcessing(JNIEnv *env,
+                                                                                   jobject instance,
+                                                                                   jlong inputImageAddr,
+                                                                                   jlong outputImageAddr) {
+
+    Mat* imageInput = (Mat*) inputImageAddr;
+    Mat* outputInput = (Mat*) outputImageAddr;
+
+    cvtColor(*imageInput, *outputInput, COLOR_RGBA2GRAY);
+
 }
--- app/src/main/java/com/tistory/deque/rectanglecorrection/rect/RectCorrectionActivity.kt
+++ app/src/main/java/com/tistory/deque/rectanglecorrection/rect/RectCorrectionActivity.kt
@@ -3,23 +3,35 @@ package com.tistory.deque.rectanglecorrection.rect
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.graphics.Bitmap
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.snackbar.Snackbar
-import com.gun0912.tedpermission.TedPermission
 import com.tedpark.tedpermission.rx2.TedRx2Permission
 import com.tistory.deque.rectanglecorrection.R
 import com.tistory.deque.rectanglecorrection.base.BaseActivity
 import com.tistory.deque.rectanglecorrection.util.EzLogger
 import com.tistory.deque.rectanglecorrection.util.RequestCodeConstant
+import com.tistory.deque.rectanglecorrection.util.getRealPath
 import kotlinx.android.synthetic.main.activity_rect_correction.*
+import org.opencv.android.Utils
+import org.opencv.core.Mat
 
 class RectCorrectionActivity : BaseActivity<RectCorrectViewModel>() {
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+            System.loadLibrary("opencv_java4")
+        }
+    }
+
     override val layoutResourceId: Int get() = R.layout.activity_rect_correction
     override val viewModel: RectCorrectViewModel
         get() = ViewModelProviders.of(this).get(RectCorrectViewModel::class.java)
+
+    private external fun loadImage(imageFileName: String, img: Long)
+    private external fun imageProcessing(inputImage: Long, outputImage: Long)
 
     override fun initStartView() {
     }
@@ -60,8 +72,21 @@ class RectCorrectionActivity : BaseActivity<RectCorrectViewModel>() {
             RequestCodeConstant.RectPictureSelectRequsetCode -> {
                 if (resultCode == Activity.RESULT_OK) {
                     data?.let {
-                        EzLogger.d("data : ${data.data}" )
-                        rect_image_view.setImageURI(data.data)
+                        EzLogger.d("uri : ${data.data}" )
+                        val imagePath = data.data?.getRealPath(this.contentResolver)
+                        EzLogger.d("path : ${imagePath}")
+
+                        val imageInput = Mat()
+                        val imageOutput = Mat()
+
+                        imagePath?.let {
+                            loadImage(it, imageInput.nativeObjAddr)
+                            imageProcessing(imageInput.nativeObjAddr, imageOutput.nativeObjAddr)
+                            val outputBitmap = Bitmap.createBitmap(imageOutput.cols(), imageOutput.rows(), Bitmap.Config.ARGB_8888)
+                            Utils.matToBitmap(imageOutput, outputBitmap)
+                            rect_image_view.setImageBitmap(outputBitmap)
+                        }
+
                     }
                 }
             }
